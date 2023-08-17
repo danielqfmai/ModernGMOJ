763b973eddba3720a33e6a3564cdd56bcf6a6e0c
danielqfmai <13928009585@163.com>
2023-08-17 22:37:39 +0800

    v1.4

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ModernGMOJ v1.3.1
+# ModernGMOJ v1.4
 
 #### [更新日志](changelog.md)
 
--- code.js
+++ code.js
@@ -11,15 +11,18 @@
 'use strict';
 
 function make_timer() {
-    var li=document.createElement('li'),a=document.createElement('a');
-    a.style.color='#0c3463';
-    a.style['font-size']='14px';
-    a.style['padding-left']='12px';
-    a.id='timer';
-    li.appendChild(a);
-    li.className='nav_bar';
-    $('#navigation')[0].appendChild(li);
-    setInterval("$('#timer')[0].innerText=(new Date($.now() + delta)).toString().substr(16,8);", 100);
+    if ($('#timer').length==0) {
+        var li=document.createElement('li'),a=document.createElement('a');
+        a.style.color='#0c3463';
+        a.style['font-size']='14px';
+        a.style['padding-left']='12px';
+        a.href='#timer';
+        a.id='timer';
+        li.appendChild(a);
+        li.className='nav_bar';
+        $('#navigation')[0].appendChild(li);
+        setInterval("$('#timer')[0].innerText=(new Date($.now() + delta)).toString().substr(16,8);", 200);
+    }
 }
 
 function remove_avatar() {
@@ -29,8 +32,6 @@ function remove_avatar() {
 }
 
 function contest_home_page() {
-    var split_by_contest=document.location.href.split('#contest');
-    if (split_by_contest.length<2||split_by_contest[1]!='') return;
     var tr=$('#contest_table tr');
     for (var i=0; i<tr.length; i++)
         tr[i].children[7].remove(),
@@ -41,48 +42,75 @@ function contest_home_page() {
 var observer = new MutationObserver(function () {
     try {
         set_page_content=function(selector, url, success) {
-            addRequest = {};
-            url = randomize(url);
-            $('.overlay').css({'z-index': '1000', 'display': 'block'});
-            $('.overlay').animate({opacity: '0.5'}, 250);
-            $.ajax({
-                type: "GET",
-                url: url,
-                success: function(data){
-                    $(selector).hide();
-                    $('.overlay').css({'z-index': '-1000', 'display': 'none'});
-                    $('.overlay').animate({opacity: '0'}, 250);
-                    $(selector).html(data);
-                    remove_avatar();
-                    make_timer();
-                    $(` #nav_task, #nav_group, #nav_ranklist, #nav_admin,
-                    #scroll_tip,
-                    #copyleft`).remove();
-                    document.body.appendChild($('#navigation')[0]);
-                    $('#nav_toggle').toggle(function() {
-                        $('#navigation').animate({ left: '-200px', width: '0' }, 320);
-                        $('#page_content').animate({ 'margin-left': '10px' }, 320);
-                        $('#icon_nav_toggle').removeClass();
-                        $('#icon_nav_toggle').addClass('icon-arrow-right');
-                    }, function() {
-                        $('#navigation').animate({ left: '0', width: '100px' }, 320);
-                        $('#page_content').animate({ 'margin-left': '110px' }, 320);
-                        $('#icon_nav_toggle').removeClass();
-                        $('#icon_nav_toggle').addClass('icon-arrow-left');
-                    });
-                    $('#nav_toggle')[0].style.top='270px';
-                    $('#nav_toggle')[0].style.left='3px';
-                    contest_home_page();
-                    $(selector).fadeIn(250);
-                    if (success != void 0) success();
-                },
-                error: function(xhr, statusText, error){
-                    $('.overlay').css({'z-index': '-1000', 'display': 'none'});
-                    $('.overlay').animate({opacity: '0'}, 250);
-                    $(selector).html('<div class="alert"><strong>Error: ' + ' ' + error + '</strong></div>');
-                }
-            });
+            if (url=='index.php/timer') {
+                $('#page_content')[0].innerHTML=`
+                <div style="
+                float: left;
+                font-size: calc(3vw - 4.8px);
+                line-height: 1.15;" id="big_time_zone">Thu Aug 17 2023</div>
+                <div style="
+                font-size: calc(25vw - 40px);
+                line-height: 1.15;
+                font-weight: 800;" id="big_timer">20:07:49</div>
+                <div style="
+                float: right;
+                font-size: calc(5vw - 8px);
+                line-height: 1.15;" id="big_date">Thu Aug 17 2023</div>`
+                $('#big_time_zone')[0].innerText=(new Date($.now() + delta)).toString().substr(25,17);
+                setInterval("$('#big_timer')[0].innerText=(new Date($.now() + delta)).toString().substr(16,8);", 200);
+                setInterval("$('#big_date')[0].innerText=(new Date($.now() + delta)).toString().substr(0,15);", 200);
+            } else {
+                addRequest = {};
+                url = randomize(url);
+                $.ajax({
+                    type: "GET",
+                    url: url,
+                    success: function(data){
+                        $(selector).hide();
+                        $(selector).html(data);
+                        remove_avatar();
+                        make_timer();
+                        $(` #nav_task, #nav_group, #nav_ranklist, #nav_admin,
+                        #scroll_tip,
+                        #copyleft`).remove();
+                        document.body.appendChild($('#navigation')[0]);
+                        $('#nav_toggle').toggle(function() {
+                            $('#navigation').animate({ left: '-200px', width: '0' }, 320);
+                            $('#page_content').animate({ 'margin-left': '10px' }, 320);
+                            $('#icon_nav_toggle').removeClass();
+                            $('#icon_nav_toggle').addClass('icon-arrow-right');
+                        }, function() {
+                            $('#navigation').animate({ left: '0', width: '100px' }, 320);
+                            $('#page_content').animate({ 'margin-left': '110px' }, 320);
+                            $('#icon_nav_toggle').removeClass();
+                            $('#icon_nav_toggle').addClass('icon-arrow-left');
+                        });
+                        $('#nav_toggle')[0].style.top='270px';
+                        $('#nav_toggle')[0].style.left='3px';
+                        contest_home_page();
+                        $(selector).fadeIn(250);
+                        if (success != void 0) success();
+                    },
+                    error: function(xhr, statusText, error){
+                        $(selector).html('<div class="alert"><strong>Error: ' + ' ' + error + '</strong></div>');
+                    }
+                });
+            }
         }
+//        init_framework=function() {
+//            	window.preventHashchange = false;
+//                if (window.location.hash != '') {
+//                    if (window.location.hash=='#timer') {
+//                    } else {
+//                        set_page_content('#page_content', hash_to_url(window.location.hash));
+//                    }
+//                }
+//                else load_page('main/home');
+//                var privilege = get_cookie('privilege');
+//                if (privilege == 'admin') $('.nav_admin').attr({style:"display:block"});
+//                else $('.nav_admin').attr({style:"display:none"});
+//                getServerPushData();
+//        }
         observer.disconnect();
     } catch(e) { }
 });
